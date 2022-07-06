6cf1ba4c40f9b9356cbff1d2edfc92cf739b405e
huGgW <75843256+huGgW@users.noreply.github.com>
2022-07-06 20:03:25 +0900

    [Bronze V] Title: A-B, Time: 216 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a;
+		int b;
+		a = s.nextInt();
+		b = s.nextInt();
+		int sum = a - b;
+		System.out.println(sum);
+	}
+}
