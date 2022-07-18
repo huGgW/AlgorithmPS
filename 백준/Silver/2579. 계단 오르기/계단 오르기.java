36a6046dcb42620492cf3bcf67a1dc834b21c26c
huGgW <75843256+huGgW@users.noreply.github.com>
2022-07-18 12:27:13 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) {
+  try {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(in.readLine());
+
+    // Initialize stairs
+    int[] stairs = new int[n+1];
+    for (int i = 1; i <= n; i++) {
+      stairs[i] = Integer.parseInt((in.readLine()));
+    }
+
+    // conOnes to record continuous one steps, and maxPoints to record maximum point on that step
+    // int[][] whatStep = new int[n+1][2];
+    int[][] maxPoints = new int[n+1][2];
+
+    // Fill the table (-1 point for forbiden cases)
+    for (int i = 1; i <= n; i++) {
+      // one stair up
+      maxPoints[i][0] = maxPoints[i-1][1] + stairs[i];
+      
+      // two stairs up
+      maxPoints[i][1] = 
+        (i >= 2) ? Math.max(maxPoints[i-2][0], maxPoints[i-2][1]) + stairs[i]
+          : stairs[i];
+    }
+
+    // Print the maximum value
+    System.out.println(Math.max(maxPoints[n][0], maxPoints[n][1]));
+  }
+  catch (IOException e) {
+    e.printStackTrace();
+  }
+  }
+}
