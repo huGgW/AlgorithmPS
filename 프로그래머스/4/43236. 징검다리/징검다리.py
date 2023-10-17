8aeef1a738205376da673f1b65583229eacef986
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2023-10-17 13:00:51 +0000

    Update 징검다리.py

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -9,11 +9,10 @@ def solution(distance: int, rocks: List[int], n: int) -> int:
     rocks.insert(0, 0)
     rocks.append(distance)
 
-    return binarySearch(rocks, 0, ceil(distance / len(rocks)), n, 0)
+    return binarySearch(rocks, 0, ceil(distance / (len(rocks)-1-n)), n, 0)
 
 
-def binarySearch(rocks: List[int], b: int, e: int, n: int, mx: int) -> int:
-    print(f"b: {b}, e: {e}, max: {mx}")
+def binarySearch(rocks: List[int], b: int, e: int, n: int, mx: int) -> int: # mx: 지금까지 결과 중 최대값 저장
     if b > e:
         return mx
 
@@ -23,9 +22,10 @@ def binarySearch(rocks: List[int], b: int, e: int, n: int, mx: int) -> int:
     if sim < 0:
         return binarySearch(rocks, b, m-1, n, mx)
     else:
-        return binarySearch(rocks, m+1, e, n, max(m, mx))
+        return binarySearch(rocks, m+1, e, n, max(m, mx)) # max(m, mx)로 결과 중 최댓값 전달
 
 
+# threshold  이상 간격이 되도록 돌의 개수 제거 (n에서 뺴는 방식으로)
 def simulate(rocks: List[int], threshold: int, n: int) -> int:
     i = 0
     for j in range(1, len(rocks)-1):
@@ -34,7 +34,7 @@ def simulate(rocks: List[int], threshold: int, n: int) -> int:
             i = j
         else:
             n -= 1
-    if rocks[len(rocks)-1] - rocks[i] < threshold:
+    if rocks[-1] - rocks[i] < threshold:
         n -= 1
 
     return n
