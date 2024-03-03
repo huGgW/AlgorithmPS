c9837239da2bf999e3a7df074596736e7f66daf3
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2024-03-03 21:25:28 +0900

    Time: 191 ms (15.16%), Space: 39.2 MB (5.33%) - LeetHub

---
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.kt
@@ -0,0 +1,45 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    var maxAbsDiff = 0
+    final val MINBOUNDARY = -1
+    final val MAXBOUNDARY = 1e5.toInt() + 1
+
+    fun maxAncestorDiff(root: TreeNode?): Int {
+        minMaxNodeTraverse(root!!)
+        return maxAbsDiff
+    }
+
+    fun minMaxNodeTraverse(node: TreeNode): Pair<Int, Int> {
+        val (leftMin, leftMax) = node.left?.let {
+            minMaxNodeTraverse(it)
+        } ?: Pair(null, null)
+
+        val (rightMin, rightMax) = node.right?.let {
+            minMaxNodeTraverse(it)
+        } ?: Pair(null, null)
+
+        val nodeMin = listOf(
+            leftMin ?: MAXBOUNDARY, rightMin ?: MAXBOUNDARY, node.`val`
+        ).min()
+
+        val nodeMax = listOf(
+            leftMax ?: MINBOUNDARY, rightMax ?: MINBOUNDARY, node.`val`
+        ).max()
+
+        maxAbsDiff = max(
+            maxAbsDiff,
+            max(nodeMax - node.`val`, node.`val` - nodeMin),
+        )
+
+        return nodeMin to nodeMax
+    }
+}
