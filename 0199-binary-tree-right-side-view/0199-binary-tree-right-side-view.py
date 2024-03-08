9509575644aa4cc23150f0609c554c1b5f77cbb4
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2024-03-08 15:53:35 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,6 +6,9 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return
+
         queue: list[tuple[TreeNode, int]] = [(root, 0)]
         result: list[int] = []
 
@@ -13,14 +16,14 @@ class Solution:
         while queue:
             node, depth = queue.pop(0)
 
-            if node is None:
-                continue
-
             if nextDepth == depth:
                 result.append(node.val)
                 nextDepth += 1
 
-            queue.append((node.right, depth + 1))
+            if node.right is not None:
+                queue.append((node.right, depth + 1))
+
+            if node.left is not None:
             queue.append((node.left, depth + 1))
 
         return result
