44d41c2ceae73094e8011a1bcb817d4c8cdef526
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2024-03-17 21:30:58 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,17 +1,30 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        maxMults = 0
-        for spl in range(2, n+1):
-            val = n // spl
-            rest = n % spl
+        # maxMults = 0
+        # for spl in range(2, n+1):
+        #     val = n // spl
+        #     rest = n % spl
+        #     maxValsOfSpl = [val for _ in range(spl)]
+        #     for i in range(rest):
+        #         maxValsOfSpl[i] += 1
+        #
+        #     mult = reduce(
+        #         lambda x, y: x * y,
+        #         maxValsOfSpl
+        #     )
+        #     maxMults = max(maxMults, mult)
+        #
+        # return maxMults
 
-            mult = 1
-            for i in range(spl):
-                mult *= val + 1 if i < rest else val
+        hist = [0, 1, 1]
+        for h in range(3, n+1):
+            maxMult = 0
+            for x in range(1, h-2):
+                y = h - x
+                xMultMax = max(x, hist[x])
+                yMultMax = max(y, hist[y])
+                mult = xMultMax * yMultMax
+                maxMult = max(mult, maxMult)
+            hist.append(maxMult)
 
-            if maxMults >= mult:
-                break
-            else:
-                maxMults = mult
-
-        return maxMults
+        return hist[-1]
