60eea49784b6b23d4598e5ceb62d8cacf468e63b
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2024-03-17 21:17:03 +0900

    Time: 43 ms (16.94%), Space: 16.5 MB (49.58%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,19 +1,14 @@
-from functools import reduce
-
 class Solution:
     def integerBreak(self, n: int) -> int:
         maxMults = 0
         for spl in range(2, n+1):
             val = n // spl
             rest = n % spl
-            maxValsOfSpl = [val for _ in range(spl)]
-            for i in range(rest):
-                maxValsOfSpl[i] += 1
 
-            mult = reduce(
-                lambda x, y: x * y,
-                maxValsOfSpl
-            )
+            mult = 1
+            for i in range(spl):
+                mult *= val + 1 if i < rest else val
+
             maxMults = max(maxMults, mult)
 
         return maxMults
