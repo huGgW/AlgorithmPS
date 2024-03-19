96890191b304b22a09b494e5811988bcacbdd001
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2024-03-19 22:00:15 +0900

    Time: 133 ms (81.16%), Space: 33.9 MB (48.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.kt
+++ 0198-house-robber/0198-house-robber.kt
@@ -1,14 +1,17 @@
 class Solution {
-    fun rob(nums: IntArray): Int {
-        val memo = IntArray(nums.size) { 0 }
-        for (i in 0 until nums.size) {
-            memo[i] = when (i) {
-                0 -> nums[0]
-                1 -> maxOf(nums[0], nums[1])
-                else -> maxOf(memo[i-1], memo[i-2] + nums[i])
+    fun rob(nums: IntArray): Int =
+        when (nums.size) {
+            1 -> nums[0]
+            2 -> maxOf(nums[0], nums[1])
+            else -> {
+                var twoBefore = nums[0]
+                var oneBefore = maxOf(nums[1], nums[0])
+                for (i in 2 until nums.size) {
+                    val next = maxOf(oneBefore, twoBefore + nums[i])
+                    twoBefore = oneBefore
+                    oneBefore = next
+                }
+                oneBefore
             }
         }
-
-        return memo[memo.size - 1]
-    }
 }
