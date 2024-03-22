0a954dd5db125e3296688f4cf972c2890228c8dd
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2024-03-23 02:09:55 +0900

    [Gold IV] Title: 최단경로, Time: 780 ms, Memory: 70480 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,47 +1,50 @@
 from typing import *
+import heapq
+import math
 import sys
-import math as m
-import heapq as hq
-
 input = sys.stdin.readline
 output = sys.stdout.write
+flush = sys.stdout.flush
 
-V, E = map(int, input().split(" "))
-startV = int(input()) 
 
-# make graph
+V, E = map(int, input().split())
+startV = int(input())
 
-graph: Dict[int, Dict[int, int]] = {}
-for i in range(1, V+1):
-    graph[i] = {}
+# init graph
+graph: Dict[int, Dict[int, int]] = {
+}
 
 for _ in range(E):
-    f, t, w = map(int, input().split(" "))
-    existE = graph[f].get(t)
-    if existE == None or existE > w:
-        graph[f][t] = w
+    u, v, w = map(int, input().split())
+    if u not in graph:
+        graph[u] = dict()
 
-# Djkstra
-minDist = {startV: 0}
-queue = []
-hq.heappush(queue, (0, startV))
-toVisit = set([i for i in range(1, V+1)])
+    if v not in graph[u]:
+        graph[u][v] = w
+    else:
+        graph[u][v] = min(w, graph[u][v])
 
-while toVisit and queue:
-    dist, targetV = hq.heappop(queue)
-    if not targetV in toVisit:
+# dijkstra
+dists = [0 if v == startV else math.inf for v in range(V+1)]
+hp = [(0, startV)]
+visited = set()
+
+while len(visited) < V and hp:
+    d, v = heapq.heappop(hp)
+    if d > dists[v]: # old one
         continue
 
-    toVisit.remove(targetV)
-
-    for neighborV in graph[targetV].keys():
-        if not neighborV in toVisit:
+    visited.add(v)
+    edges = graph.get(v, {})
+    for (u, w) in edges.items():
+        if u in visited:
             continue
-        distAfterTarget = dist + graph[targetV][neighborV]
-        if distAfterTarget < minDist.get(neighborV, m.inf):
-            hq.heappush(queue, (distAfterTarget, neighborV))
-            minDist[neighborV] = distAfterTarget
+        updateDist = d + w
+        if updateDist < dists[u]:
+            heapq.heappush(hp, (updateDist, u))
+            dists[u] = updateDist
 
-# print output
-for v in range(1, V+1):
-    output(f"{minDist.get(v, 'INF')}\n")
+# print dists
+for i in range(1, V+1):
+    output(f"{'INF' if dists[i] == math.inf else dists[i]}\n")
+flush()
