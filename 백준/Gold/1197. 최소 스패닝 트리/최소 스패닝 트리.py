0d81e5787d57b79e9e2eacf449be2f668591e395
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2024-03-23 02:40:58 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 500 ms, Memory: 61752 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,48 +1,41 @@
 from typing import *
-from queue import PriorityQueue
 import sys
+import heapq
+
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-# (Dist, From, To)
-edges = PriorityQueue()
+V, E = map(int, input().split())
+graph = {
+    v: {} for v in range(1, V+1)
+}
 
-N, M = map(int, input().split(" "))
-for _ in range(M):
-    f, t, d = map(int, input().split(" "))
-    edges.put((d, f, t))
-
-groups: List[Set[int]] = []
-def findGroupIdx(x: int) -> int:
-    for i in range(len(groups)):
-        if x in groups[i]:
-            return i
-
-    return -1
-
-def mergeGroup(idx1: int, idx2: int):
-    global groups
-    smallIdx = min(idx1, idx2)
-    bigIdx = max(idx1, idx2)
-    groups[smallIdx] = groups[smallIdx].union(groups[bigIdx])
-    del groups[bigIdx]
-
-dist = 0
-while not groups or len(groups[0]) < N:
-    d, f, t = edges.get()
-    fIdx = findGroupIdx(f)
-    tIdx = findGroupIdx(t)
-
-    if fIdx == -1 and tIdx == -1:
-        groups.append({f, t})
-    elif fIdx == -1 and tIdx != -1:
-        groups[tIdx].add(f)
-    elif fIdx != -1 and tIdx == -1:
-        groups[fIdx].add(t)
-    elif fIdx != tIdx:
-        mergeGroup(fIdx, tIdx)
+for _ in range(E):
+    f, t, c = map(int, input().split())
+    if t in graph[f]:
+        graph[f][t] = min(graph[f][t], c)
+        graph[t][f] = min(graph[t][f], c)
     else:
-        continue
+        graph[f][t] = c
+        graph[t][f] = c
 
-    dist += d
+# Prim's algorithm
+hp = []
+visited = {1}
+nextV = 1
+weightSum = 0
+while len(visited) < V:
+    edges = graph[nextV]
+    for u, w in edges.items():
+        if u not in visited:
+            heapq.heappush(hp, (w, u))
 
-print(dist)
+    while True:
+        ww, uu = heapq.heappop(hp)
+        if uu not in visited:
+            visited.add(uu)
+            nextV = uu
+            weightSum += ww
+            break
+
+print(weightSum)
