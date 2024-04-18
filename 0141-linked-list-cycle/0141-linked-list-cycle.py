f45ba61ee35a993bcb3b7b092cc6de64a7395a12
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2024-04-18 13:41:36 +0900

    Time: 54 ms (11.31%), Space: 19.2 MB (18.42%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,11 +6,19 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        history = set()
-        while head is not None:
-            if head in history:
+        fast = head
+        tail = head
+
+        while fast is not None and tail is not None:
+            fast = fast.next
+            if fast is not None:
+                fast = fast.next
+            else:
+                break
+            
+            tail = tail.next
+
+            if fast is not None and tail is not None and fast is tail:
                 return True
-            history.add(head)
-            head = head.next
 
         return False
