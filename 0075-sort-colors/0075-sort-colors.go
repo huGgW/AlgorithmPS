70d67100229a28a6a0bcf71fa3113a76daac1d93
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2024-05-17 19:38:39 +0900

    Time: 1 ms (76.02%), Space: 2.3 MB (83.39%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.go
+++ 0075-sort-colors/0075-sort-colors.go
@@ -1,20 +1,22 @@
 func sortColors(nums []int) {
-	zeros, ones, twos := 0, 0, 0
-	for _, n := range nums {
-		switch n {
-		case 0: zeros++
-		case 1: ones++
-		case 2: twos++
-		}
-	}
+	zeroBoundary, twoBoundary := -1, len(nums)
 
-	for i := range zeros {
-		nums[i] = 0
-	}
-	for i := range ones {
-		nums[zeros + i] = 1
-	}
-	for i := range twos {
-		nums[zeros + ones + i] = 2
+	i := 0
+	for zeroBoundary < i && i < twoBoundary {
+		n := nums[i]
+		switch n {
+		case 0:
+			zeroBoundary++
+			nums[i], nums[zeroBoundary] = nums[zeroBoundary], nums[i]
+			if i <= zeroBoundary {
+				i = zeroBoundary + 1
+			}
+
+		case 2:
+			twoBoundary--
+			nums[i], nums[twoBoundary] = nums[twoBoundary], nums[i]
+		default:
+			i++
+		}
 	}
 }
