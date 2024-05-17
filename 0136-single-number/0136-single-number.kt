72a10ddbe42ea2c2e39224ffa1a9fc034be7a274
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2024-05-17 22:13:59 +0900

    Time: 220 ms (64.38%), Space: 39.3 MB (55.69%) - LeetHub

--- 0136-single-number/0136-single-number.kt
+++ 0136-single-number/0136-single-number.kt
@@ -1,13 +1,4 @@
 class Solution {
-    fun singleNumber(nums: IntArray): Int {
-        val set = mutableSetOf<Int>()
-        nums.forEach {
-            if (set.contains(it)) {
-                set.remove(it)
-            } else {
-                set.add(it)
-            }
-        }
-        return set.first()
-    }
+    fun singleNumber(nums: IntArray): Int
+        = nums.fold(0) { l, r -> l xor r }
 }
