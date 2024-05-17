d7596bae3913e5a391764fe0d80c3aa585163360
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2024-05-17 16:11:08 +0900

    Time: 341 ms (38.2%), Space: 51.8 MB (14.02%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.kt
+++ 0056-merge-intervals/0056-merge-intervals.kt
@@ -1,27 +1,20 @@
-import java.util.PriorityQueue
-import kotlin.math.*
-
 class Solution {
     fun merge(intervals: Array<IntArray>): Array<IntArray> {
-        val pq = PriorityQueue<IntArray> { 
-            a, b -> b[1] - a[1]
-        }
-        intervals.sortBy { it[0] }
+        val ans = mutableListOf<IntArray>()
+        intervals.sortWith(compareBy({it[0]}, {it[1]}))
 
         intervals.forEach {
-            if (pq.isEmpty()) {
-                pq.offer(it)
+            val (_, topE) = ans.lastOrNull() ?: let { _ ->
+                ans.add(it)
+                return@forEach
+            }
+            if (topE >= it[0]) {
+                ans.last()[1] = max(topE, it[1])
             } else {
-                val (_, topE) = pq.peek()
-                val (b, e) = it
-                if (topE >= b) {
-                    pq.peek()[1] = max(e, topE)
-                } else {
-                    pq.offer(it)
-                }
+                ans.add(it)
             }
         }
 
-        return pq.toArray(arrayOf())
+        return ans.toTypedArray()
     }
 }
