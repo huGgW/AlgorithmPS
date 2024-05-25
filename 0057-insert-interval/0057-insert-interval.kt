34c70077a24d6435915cc163290a834e3c0ff4c6
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2024-05-25 18:24:10 +0900

    Time: 243 ms (58.36%), Space: 42.1 MB (5.57%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.kt
+++ 0057-insert-interval/0057-insert-interval.kt
@@ -1,45 +1,34 @@
 class Solution {
     fun insert(intervals: Array<IntArray>, newInterval: IntArray): Array<IntArray> {
-        val (begin, end) = let {
-            var b: Int? = null
-            var e: Int? = null
-            intervals.forEachIndexed { i, interval ->
-                if (!(interval[1] < newInterval[0] || newInterval[1] < interval[0])) {
-                    b ?: let { b = i }
-                    e = i
-                }
+        val ans = mutableListOf<IntArray>()
+        var inserted = false
+
+        var i = 0
+        while (i < intervals.size) {
+            val isNewInterval = !inserted && newInterval[0] < intervals[i][0]
+            val nextInterval = if (isNewInterval) { newInterval } else { intervals[i] }
+
+            if (ans.isEmpty() || ans.last()[1] < nextInterval[0] ) {
+                ans.add(nextInterval)
+            } else {
+                ans.last()[1] = maxOf(ans.last()[1], nextInterval[1])
             }
-            b to e
+
+            if (isNewInterval) {
+                inserted = true
+            } else {
+                i++
+            }
         }
 
-        return if (begin == null && end == null) {
-            intervals.toMutableList().run {
-                val idx = -1 * binarySearch(newInterval, { a, b -> a[0] - b[0] }) - 1
-                add(idx, newInterval)
-                toTypedArray()
+        if (!inserted) {
+            if (ans.isEmpty() || ans.last()[1] < newInterval[0] ) {
+                ans.add(newInterval)
+            } else {
+                ans.last()[1] = maxOf(ans.last()[1], newInterval[1])
             }
-        } else if (begin != null && end != null) {
-            val ans = mutableListOf<IntArray>()
-            val merged = mutableListOf<Int>()
-
-            intervals.forEachIndexed { i, interval ->
-                if (i == begin) {
-                    merged.add(minOf(interval[0], newInterval[0]))
-                }
-
-                if (i == end) {
-                    merged.add(maxOf(interval[1], newInterval[1]))
-                    ans.add(merged.toIntArray())
-                }
-
-                if (i !in begin..end) {
-                    ans.add(interval)
-                }
-            }
-
-            ans.toTypedArray()
-        } else {
-            throw Exception()
         }
+
+        return ans.toTypedArray()
     }
 }
