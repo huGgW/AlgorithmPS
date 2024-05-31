d5b3c047d0a9434df4942f3c72a0057180bb1ede
우혁준 (HyukJoon Woo) <whjoon0225@naver.com>
2024-06-01 03:04:39 +0900

    Time: 2430 ms (5.13%), Space: 53.5 MB (5.13%) - LeetHub

--- 0322-coin-change/0322-coin-change.kt
+++ 0322-coin-change/0322-coin-change.kt
@@ -3,44 +3,47 @@ data class State (
     val amount: Int,
 )
 
-
 class Solution {
     fun coinChange(coins: IntArray, amount: Int): Int {
         coins.sortDescending()
         return lookUp(coins, State(0, amount), mutableMapOf())
     }
 
-    fun lookUp(coins: IntArray, state: State, history: MutableMap<State, Int>): Int = history[state] ?: let {
-        if (state.nextIndex == coins.size - 1) {
-            val cnt = if (state.amount % coins.last() == 0) {
-                state.amount / coins.last()
-            } else {
-                -1
-            }
-
-            cnt.also {
-                history[state] = it
-            }
-
-        } else {
-            val coin = coins[state.nextIndex]
-            var leftAmount = state.amount % coin
-            var thisCnt = state.amount / coin
-            var minTotalCnt: Int = -1
-
-            while (thisCnt >= 0) {
-                val cnt = lookUp(coins, State(state.nextIndex + 1, leftAmount), history)
-                if (cnt != -1) {
-                    if (minTotalCnt == -1 || minTotalCnt > cnt + thisCnt) {
-                        minTotalCnt = cnt + thisCnt
+    fun lookUp(coins: IntArray, state: State, history: MutableMap<State, Int>): Int
+    = history[state] ?: let {
+        when (state.nextIndex) {
+            coins.size - 1 -> {
+                val cnt = if (state.amount % coins.last() == 0) {
+                        state.amount / coins.last()
+                    } else {
+                        -1
                     }
-                }
-                thisCnt--
-                leftAmount += coin
-            }
 
-            minTotalCnt.also {
-                history[state] = it
+                cnt.also {
+                        history[state] = it
+                    }
+            }
+            else -> {
+                val coin = coins[state.nextIndex]
+                var leftAmount = state.amount % coin
+                var thisCnt = state.amount / coin
+                var minTotalCnt: Int = -1
+
+                while (thisCnt >= 0) {
+                    val cnt = lookUp(coins, State(state.nextIndex + 1, leftAmount), history)
+                    if (cnt != -1) {
+                        if (minTotalCnt == -1 || minTotalCnt > cnt + thisCnt) {
+                            minTotalCnt = cnt + thisCnt
+                        }
+                    }
+                    thisCnt--
+                    leftAmount += coin
+                }
+
+                minTotalCnt.also {
+                    history[state] = it
+                }
+
             }
         }
     }
